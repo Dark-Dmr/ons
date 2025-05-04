8b9446a35a2ec3f1e8962e9ddf5b4bf83af28a19
jehad <jehad-alhazmi@outlook.com>
2025-05-04 11:06:51 +0300

    creare resourse and add some futre in api

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+
+class Admin extends Authenticatable
 {
-    //
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    protected $hidden = [
+        'password',
+    ];
+    protected $table = 'admins';
 }
